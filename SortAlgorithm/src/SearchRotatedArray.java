b2f21193fc492ded7a0e2f6be4d866c0aec43c48
sowmyarao02 <41974016+sowmyarao02@users.noreply.github.com>
2018-08-10 20:01:02 -0700

    Update SearchRotatedArray.java

--- SortAlgorithm/src/SearchRotatedArray.java
+++ SortAlgorithm/src/SearchRotatedArray.java
@@ -1,3 +1,7 @@
+/*
+Given a sorted array of n integers that has been rotated an unknown number of times, 
+write code to find an element in the array. You may asume that the array was originally sorted in increasing Order.
+*/
 
 public class SearchRotatedArray {
 
