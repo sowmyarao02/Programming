7a0be4516db3e535a94975b782a5c6afcfe5cb3f
sowmyarao02 <41974016+sowmyarao02@users.noreply.github.com>
2018-08-11 15:13:42 -0700

    Search a string array interspersed with empty strings

---
+++ SortAlgorithm/src/SearchStringArray.java
@@ -0,0 +1,54 @@
+
+public class SearchStringArray {
+	
+	
+	public static void main(String[] str) {
+		
+		String[] a = new String[] {"at","","","","ball",
+				"","","cat","","","dad","",""};
+		
+	//	String k = "ball"; // STRING FOUND AT --- 4
+	//	String k = "cat";  // STRING FOUND AT --- 7
+	//	String k = "dad";  // STRING FOUND AT --- 10
+		String k = "cat";   // STRING FOUND AT --- 7
+		
+		SearchStringArray s = new SearchStringArray();
+		int r = s.bSearch(a, k, 0, a.length-1);
+		
+		if(r == -1)
+			System.out.println("### STRING NOT FOUND ###");
+		else
+			System.out.println("STRING FOUND AT --- "+r);
+		
+	}
+	
+	public int bSearch(String[] a, String k,int f,int l) {
+		
+		int m = (f+l)/2;
+		int m2=m;
+		
+		while(m2>=f && "".equals(a[m2]) ) {
+			m2--;
+		}
+		
+		if(m2>=f) {
+		if(k.equals(a[m2])) {
+			return m2;	
+		}
+		else if(k.compareTo(a[m2])<0) {
+			return bSearch(a,k,f,m2-1);
+		}
+		else if(k.compareTo(a[m2])>0) {
+			return bSearch(a,k,m+1,l);
+			}
+		}
+		else {
+			return bSearch(a,k,m+1,l);
+		}
+		
+		return -1;
+	}
+	
+	
+
+}
