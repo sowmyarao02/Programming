e976b2a0cab867a9ca58764afba4d1ee80b2b553
sowmyarao02 <41974016+sowmyarao02@users.noreply.github.com>
2018-08-14 18:41:07 -0700

    Update BinarySearchTree.java

--- SortAlgorithm/src/BinarySearchTree.java
+++ SortAlgorithm/src/BinarySearchTree.java
@@ -1,3 +1,14 @@
+/*
+
+Imagine you are reading in a stream of integers. Periodically, you wish to be able to look up the rank of a 
+number x (the number of values less than or equal to x). Implement the data structures and algorithms to support
+these operations. That is, implement the method track(int x), which is called when each number is generated, and 
+the methos getRankOfNumber(int x), which returns the number of values less than or equal to x
+(not including x itself)
+
+
+
+*/
 
 public class BinarySearchTree {
 
